1f9f2ef408d905fc207e2ce63ff6c0ebd66939f1
TulioGuaraldoB <tulio.brugin@fatec.sp.gov.br>
2022-02-23 22:08:16 -0300

    end-of-work

---
+++ animations/goback.js
@@ -0,0 +1,5 @@
+window.setTimeout(
+    function () {
+        window.location.replace('../index.html');
+    }, 6000 // 6 seconds
+);
--- validation.js
+++ validation.js
@@ -11,5 +11,5 @@
 
                 form.classList.add('was-validated')
             }, false)
-        })
+        })    
 })()
